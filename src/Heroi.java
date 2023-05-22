00eeb7a9d445be1ec42cc0bc039a987dbed7243d
Tiago Paulin <127238987+TiagoPaulin@users.noreply.github.com>
2023-05-22 19:34:16 -0300

    verificação do tipo de Heroi

--- src/Heroi.java
+++ src/Heroi.java
@@ -1,19 +1,47 @@
 public class Heroi extends Personagem {
+//    Criando os atributos da classe
     private String nome;
-
     private String tipo;
-
     private int ouro;
 
-    public Heroi(int vida, int mana, int defesa, int inteligencia, int forca, String nome, String tipo, int ouro) {
-        super(vida, mana, defesa, inteligencia, forca);
+//    Criando construtor da classe
+    public Heroi(String nome, String tipo) {
+//      chamando construtor da classe pai com valores zerados
+        super(0, 0, 0, 0, 0, 0);
+
         this.nome = nome;
         this.tipo = tipo;
-        this.ouro = ouro;
+//      seta os atributos da classe pai com novos valores baseado no tipo de heroi
+        if (tipo.equals("Guerreiro")) {
+            super.setVida(60);
+            super.setMana(10);
+            super.setDefesa(10);
+            super.setInteligencia(4);
+            super.setForca(11);
+            super.setAgilidade(5);
+            this.ouro = 20;
+        } else if (tipo.equals("Arqueiro")) {
+            super.setVida(50);
+            super.setMana(15);
+            super.setDefesa(8);
+            super.setInteligencia(6);
+            super.setForca(7);
+            super.setAgilidade(12);
+            this.ouro = 20;
+        } else if (tipo.equals("Mago")) {
+            super.setVida(45);
+            super.setMana(35);
+            super.setDefesa(6);
+            super.setInteligencia(13);
+            super.setForca(4);
+            super.setAgilidade(8);
+            this.ouro = 20;
+        }
     }
 
+//    implementacao do metodo abstrato da classe pai
     @Override
     public String falaPersonagem(){
-        return "......";
+        return "mil meu com mil teu...";
     }
 }
--- src/Personagem.java
+++ src/Personagem.java
@@ -1,62 +1,69 @@
 import java.util.ArrayList;
 
 public abstract class Personagem {
+//    Criacao dos atributos da classe
     private int vida;
-
     private int mana;
-
     private int defesa;
-
     private int inteligencia;
-
     private int forca;
-
-    private Arma arma;
+    private int agilidade;
+//    private Arma arma;
     private Armadura armadura;
 
-    public Personagem(int vida, int mana, int defesa, int inteligencia, int forca) {
+//    criacao do construtor
+    public Personagem(int vida, int mana, int defesa, int inteligencia, int forca, int agilidade) {
         this.vida = vida;
         this.mana = mana;
         this.defesa = defesa;
         this.inteligencia = inteligencia;
         this.forca = forca;
+        this.agilidade = agilidade;
     }
 
+//    Implementacao dos metodos get
     public int getVida() {
         return vida;
     }
-
     public int getMana() {
         return mana;
     }
-
     public int getDefesa() {
         return defesa;
     }
-
     public int getInteligencia() {
         return inteligencia;
     }
-
     public int getForca() {
         return forca;
     }
-
-    public Arma getArma() {
-        return arma;
-    }
-
+//    public Arma getArma() {
+//        return arma;
+//    }
     public Armadura getArmadura() {
         return armadura;
     }
 
+//    Implementacao dos metodos set
     public void setVida(int vida) {
         this.vida = vida;
     }
-
     public void setMana(int mana) {
         this.mana = mana;
     }
+    public void setDefesa(int defesa) {
+        this.defesa = defesa;
+    }
+    public void setInteligencia(int inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+    public void setForca(int forca) {
+        this.forca = forca;
+    }
+    public void setAgilidade(int agilidade) {
+        this.agilidade = agilidade;
+    }
 
+//    cricao do metodo abstrato(sem implementacao)
     public abstract String falaPersonagem();
 }
