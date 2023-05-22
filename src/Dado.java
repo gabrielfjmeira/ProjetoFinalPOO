9a364248c917fe134e6ff638552aaffcb1f884b7
Thales <127799349+ThalesYa@users.noreply.github.com>
2023-05-22 11:45:16 -0300

    Dungeon e Dado

---
+++ src/Dado.java
@@ -0,0 +1,9 @@
+package ProjetoFinalPOO.src;
+
+public class Dado {
+    private int n_lados;
+
+    public Dado(int n_lados) {
+        this.n_lados = n_lados;
+    }
+}
---
+++ src/Dungeon.java
@@ -0,0 +1,12 @@
+package ProjetoFinalPOO.src;
+
+public class Dungeon {
+    private String nome;
+    private int n_inimigos;
+
+    public Dungeon(String nome, int n_inimigos) {
+        this.nome = nome;
+        this.n_inimigos = n_inimigos;
+    }
+
+}
