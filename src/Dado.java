02f9e6cce9d36fbe298dcaf551ba48cc621e7a64
Thales <127799349+ThalesYa@users.noreply.github.com>
2023-05-23 19:33:46 -0300

    Add files via upload
    Nova implementação do dado

--- src/Dado.java
+++ src/Dado.java
@@ -1,9 +1,15 @@
-package ProjetoFinalPOO.src;
+import java.util.Random;
 
 public class Dado {
     private int n_lados;
 
     public Dado(int n_lados) {
-        this.n_lados = n_lados;
+        this.n_lados = 20;
     }
+
+    public int lancarDado(){
+        Random random = new Random();
+        return random.nextInt(n_lados) + 1;
+    }
+
 }
